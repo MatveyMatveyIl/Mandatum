55b64b6b57f4fef4c58fd43b33a0361118f3adbc
MatveyMatveyIl <matvey.ilichev@gmail.com>
2021-11-30 20:06:56 +0500

    Convertor Task upd

--- Mandatum/UI/Convertors/TaskModelConverter.cs
+++ Mandatum/UI/Convertors/TaskModelConverter.cs
@@ -10,15 +10,41 @@ namespace Mandatum.Convertors
             return new TaskRecord()
             {
                 Description = source.Description,
-                Status = source.Status,
+                Status = ConvertToTaskRecordStatus(source.Status),
                 Term = source.Term,
                 Topic = source.Topic,
             };
         }
 
+        private TaskStatusRecord ConvertToTaskRecordStatus(TaskStatusModel statusRecord)
+        {
+            return statusRecord switch
+            {
+                TaskStatusModel.Done => TaskStatusRecord.Done,
+                TaskStatusModel.InProgress => TaskStatusRecord.InProgress,
+                TaskStatusModel.InWait => TaskStatusRecord.InWait
+            };
+        }
+        
+        private TaskStatusModel ConvertToTaskModelStatus(TaskStatusRecord statusRecord)
+        {
+            return statusRecord switch
+            {
+                TaskStatusRecord.Done => TaskStatusModel.Done,
+                TaskStatusRecord.InProgress => TaskStatusModel.InProgress,
+                TaskStatusRecord.InWait => TaskStatusModel.InWait
+            };
+        }
+
         public TaskModel Convert(TaskRecord source)
         {
-            throw new System.NotImplementedException();
+            return new TaskModel()
+            {
+                Description = source.Description,
+                Status = ConvertToTaskModelStatus(source.Status),
+                Term = source.Term,
+                Topic = source.Topic,
+            };
         }
     }
 }
