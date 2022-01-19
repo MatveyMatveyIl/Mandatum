2dc2f1a3d7a066fbcea8d7f03670effc61c4e167
iulpv <yul_karpova@mail.ru>
2022-01-19 16:13:53 +0500

    ++1

--- Mandatum/Tests/UnitTest1.cs
+++ Mandatum/Tests/UnitTest1.cs
@@ -187,6 +187,32 @@ namespace Tests
             boards += 2;
             Assert.AreEqual(board.Count, boards);
         }
+        [Test]
+        public void AddNewUsersOnBoard()
+        {
+            var serviceCollection = new ServiceCollection();
+            IEnumerable<KeyValuePair<string, string>> confValues = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("TransactionLogging:Enabled", "True"),
+                new KeyValuePair<string, string>("ConnectionStrings:DefaultConnection", "Data Source=mandatumdasha.database.windows.net;Initial Catalog=Mandatum;Persist Security Info= True; User Id=dasha;Password=Mandatum2038")
+            };
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddInMemoryCollection(confValues);
+            var confRoot = builder.Build();
+            
+            var startUp = new Startup(confRoot);
+            startUp.ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var boardApi = serviceProvider.GetRequiredService<IBoardApi>();
+            var userApi = serviceProvider.GetRequiredService<IUserRepo>();
+            boardApi.DeleteBoard(new Guid("2991DA4F-252B-4038-B2B8-E0728BDE3BFE"));
+            var boards = userApi.GetUser("dshzhkv").Boards.Count;
+            boardApi.CreateBoard(new BoardRecord(){Id = new Guid("2991DA4F-252B-4038-B2B8-E0728BDE3BFE"), Name = "3"}, "iulpv");
+            boardApi.AddNewUserToBoard("dshzhkv", new Guid("2991DA4F-252B-4038-B2B8-E0728BDE3BFE"));
+            var boardUser = userApi.GetUser("dshzhkv").Boards;
+            boards += 1;
+            Assert.AreEqual(boardUser.Count, boards);
+        }
         
     }
 
